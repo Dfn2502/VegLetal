624448aa16fe2642cf05e176a1cc678628f18831
Dfn2502 <115895102+Dfn2502@users.noreply.github.com>
2026-05-23 22:37:32 -0600

    Pantalla Victoria

--- Assets/Scripts/GameOverUI.cs
+++ Assets/Scripts/GameOverUI.cs
@@ -4,10 +4,20 @@ using UnityEngine.UI;
 public class GameOverUI : MonoBehaviour
 {
     public Text textoScore;
-
+    public CambioEscena cambioEscena;
     void Start()
     {
         int score = PlayerPrefs.GetInt("ScoreFinal", 0);
         textoScore.text = "Score: " + score;
+    }
+
+    public void ReiniciarJuego()
+    {
+        cambioEscena.CambiarEscena("EscenaJuego");
+    }
+
+    public void VolverAlMenu()
+    {
+        cambioEscena.CambiarEscena("EscenaInicio");
     }
 }
